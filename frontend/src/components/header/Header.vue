be74069736c6c6996de091498c73e016f2794ded
luiza <luizaxakieva@gmail.com>
2025-10-28 13:53:38 +0300

    Добавление верстки для промо страницы

---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <img
+        src=""
+        height="30"
+        width="30"
+        alt="Логотип."
+    />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import PromoMainPage from '@/views/PromoMainPage.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Promo',
+            component: PromoMainPage,
+        },
+    ],
+});
 
-export default router
+export default router;
