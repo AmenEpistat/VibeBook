d5d4fe50a95c2e397c0d6d757849601aba95ea34
luiza <luizaxakieva@gmail.com>
2025-12-12 17:44:49 +0300

    adds the author card component

---
+++ frontend/src/components/AuthorCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="author-card">
+        <p class="authors-page__name">{{ author.name }} {{ author.surname }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Author } from '@/types/author.ts';
+
+const props = defineProps<{
+    author: Author;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
