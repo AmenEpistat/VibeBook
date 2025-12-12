1bbbd60a494463659a0218999aed8ad2b0ef9475
luiza <luizaxakieva@gmail.com>
2025-12-12 17:58:49 +0300

    adds the genre card component

---
+++ frontend/src/components/GenreCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="genre-card">
+        <p class="genres-page__name">{{ genre.name }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Genre } from '@/types/genre.ts';
+
+const props = defineProps<{
+    genre: Genre;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
