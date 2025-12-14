4039c5e8ee9c87ee1ba038264b4980f9c36cbaed
luiza <luizaxakieva@gmail.com>
2025-12-14 18:54:04 +0300

    adds a component for admin actions

---
+++ frontend/src/components/AdminAction.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="admin-action">
+        <v-btn @click="editItem">
+            <v-icon left>mdi-pencil</v-icon>
+        </v-btn>
+
+        <v-btn @click="deleteItem">
+            <v-icon left>mdi-delete</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['edit', 'delete']);
+
+const editItem = () => {
+    emit('edit');
+};
+
+const deleteItem = () => {
+    emit('delete');
+};
+</script>
+
+<style scoped lang="scss"></style>
