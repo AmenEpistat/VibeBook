6aa3c3d551c2fb58410e38a6904cd3f6aa3b8c41
luiza <luizaxakieva@gmail.com>
2025-12-14 18:54:57 +0300

    adds a component for the user's appearance

---
+++ frontend/src/components/UserCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <article class="user-card">
+        <h3 class="user-card__name">{{ user.username }}</h3>
+        <p class="user-card__email">{{ user.email }}</p>
+        <p class="user-card__role">{{ user.roles?.[0] }}</p>
+        <AdminAction
+            @edit="makeAdmin"
+            @delete="() => {}"
+        />
+    </article>
+</template>
+
+<script setup lang="ts">
+import type { IAuth } from '@/types/auth.ts';
+import AdminAction from '@/components/AdminAction.vue';
+
+const props = defineProps<{
+    user: IAuth;
+}>();
+const emit = defineEmits(['make-admin', 'delete']);
+
+const makeAdmin = () => {
+    emit('make-admin', props.user._id);
+};
+</script>
+
+<style scoped lang="scss"></style>
