a69d9cf757faf517b4850a24dad5f405df617eec
luiza <luizaxakieva@gmail.com>
2025-12-16 21:50:11 +0300

    edits methods because of a useRequest

--- frontend/src/stores/bookStore.ts
+++ frontend/src/stores/bookStore.ts
@@ -1,64 +1,39 @@
 import { defineStore } from 'pinia';
-import type { Book, CreateIBook } from '@/types/book.ts';
-import { ref } from 'vue';
-import BookService from '@/services/BookService.ts';
+import { ref, computed } from 'vue';
+import type { Book, CreateIBook } from '@/types/book';
+import BookService from '@/services/BookService';
+import { useUserBookStore } from '@/stores/userBookStore';
+import { useRequest } from '@/composables/useRequest.ts';
+import type { UserBook } from '@/types/user.ts';
 
 export const useBookStore = defineStore('book', () => {
     const books = ref<Book[]>([]);
-    const isLoading = ref(true);
-    const errorMessage = ref('');
+    const { isLoading, errorMessage, fetch } = useRequest<UserBook[]>();
+
+    const userBookStore = useUserBookStore();
+
+    const booksWithState = computed(() => {
+        return books.value.map((book) => ({
+            ...book,
+            userState: userBookStore.userBookMap[book._id],
+        }));
+    });
 
     const getBooks = async () => {
-        try {
-            const response = await BookService.getBooks();
-            books.value = response.data;
-            console.log(response.data);
-        } catch (e) {
-            console.log(e.response?.data?.message);
-            errorMessage.value = e.response?.data?.message;
-        } finally {
-            isLoading.value = false;
-        }
+        books.value = await fetch(BookService.getBooks);
     };
 
     const getBook = async (id: string) => {
-        try {
-            isLoading.value = true;
-            const response = await BookService.getBook(id);
-            return response.data;
-        } catch (e) {
-            console.log(e.response?.data?.message);
-            errorMessage.value = e.response?.data?.message;
-        } finally {
-            isLoading.value = false;
-        }
+        return await fetch(BookService.getBook, id);
     };
 
     const createBook = async (book: CreateIBook) => {
-        try {
-            isLoading.value = true;
-            const response = await BookService.createBook(book);
-            return response.data;
-        } catch (e) {
-            console.log(e.response?.data?.message);
-            errorMessage.value = e.response?.data?.message;
-        } finally {
-            isLoading.value = false;
-        }
+        return await fetch(BookService.createBook, book);
     };
 
     const updateBook = async (book: CreateIBook, id: string) => {
-        try {
-            isLoading.value = true;
-            const response = await BookService.updateBook(book, id);
-            return response.data;
-        } catch (e) {
-            console.log(e.response?.data?.message);
-            errorMessage.value = e.response?.data?.message;
-        } finally {
-            isLoading.value = false;
-        }
+        return await fetch(BookService.updateBook, book, id);
     };
 
-    return { books, isLoading, errorMessage, getBooks, getBook, createBook, updateBook };
+    return { books, booksWithState, isLoading, errorMessage, getBooks, getBook, createBook, updateBook };
 });
