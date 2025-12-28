076741c18c001f7f6c9433b69b1d4eb7fbd11f5f
luiza <luizaxakieva@gmail.com>
2025-12-28 13:43:14 +0300

    adds the 404-page

---
+++ frontend/src/views/NotFoundPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="not-found content-wrapper">
+        <h1>404</h1>
+        <p>Страница не найдена</p>
+
+        <RouterLink to="/">На главную</RouterLink>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
