c332d2487dcddede7c257e4d6f1f7073f054a66b
luiza <luizaxakieva@gmail.com>
2026-01-07 21:16:00 +0300

    adds styles to the 404 page

--- frontend/src/views/NotFoundPage.vue
+++ frontend/src/views/NotFoundPage.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="not-found content-wrapper">
-        <h1>404</h1>
-        <p>Страница не найдена</p>
-
-        <RouterLink to="/">На главную</RouterLink>
+        <v-btn
+            class="primary-button"
+            @click="router.push('/')"
+            >На главную</v-btn
+        >
+        <img
+            src="@/assets/img/404.jpg"
+            alt="Неправильная ссылка"
+            width="100"
+            height="100"
+            class="not-found__img"
+        />
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from '@/router/index.ts';
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.not-found__img {
+    width: 100%;
+    height: 100%;
+}
+</style>
