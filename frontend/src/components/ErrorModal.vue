0c5b23a312a1a459a782a1b6b73d52ef53637589
luiza <luizaxakieva@gmail.com>
2026-01-17 13:59:34 +0300

    adds an error dialog component for error detection

---
+++ frontend/src/components/ErrorModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-dialog
+        :model-value="modelValue"
+        max-width="400"
+        persistent
+        transition="dialog-bottom-transition"
+        @update:model-value="emit('update:modelValue', $event)"
+    >
+        <v-card>
+            <v-card-title class="text-h6 text-center"> Ошибка </v-card-title>
+
+            <v-card-text class="text-center">
+                {{ error }}
+            </v-card-text>
+
+            <v-card-actions class="justify-center">
+                <v-btn
+                    color="primary"
+                    rounded="xs"
+                    @click="close"
+                >
+                    Понятно
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    modelValue: boolean;
+    error: string;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void;
+}>();
+
+function close() {
+    emit('update:modelValue', false);
+}
+</script>
