b9dbe13adee3eb7ecbba3a1da9ca2e0461b2268b
luiza <luizaxakieva@gmail.com>
2026-01-17 15:27:51 +0300

    edits fetch for the delayed function

--- frontend/src/composables/useRequest.ts
+++ frontend/src/composables/useRequest.ts
@@ -1,14 +1,17 @@
 import { ref } from 'vue';
+import { withMinDelay } from '@/utils/delay.ts';
 
 export function useRequest<T>() {
     const isLoading = ref(false);
-    const errorMessage = ref('');
+    const errorMessage = ref<string | null>(null);
     const data = ref<T>();
 
     const fetch = async (fn: (...args: any[]) => Promise<any>, ...args: any[]) => {
         try {
+            errorMessage.value = null;
             isLoading.value = true;
-            const response = await fn(...args);
+
+            const response = await withMinDelay(() => fn(...args), 1000);
             data.value = response.data;
             return response.data;
         } catch (e: any) {
