ee65a621b4babf0a3513a7bbe56f6129ed2d6a4e
luiza <luizaxakieva@gmail.com>
2026-01-18 18:52:35 +0300

    adds methods with errorstore

--- frontend/src/composables/useRequest.ts
+++ frontend/src/composables/useRequest.ts
@@ -1,26 +1,28 @@
 import { ref } from 'vue';
 import { withMinDelay } from '@/utils/delay.ts';
+import { useErrorStore } from '@/stores/errorStore.ts';
 
 export function useRequest<T>() {
     const isLoading = ref(false);
-    const errorMessage = ref<string | null>(null);
+    const errorStore = useErrorStore();
     const data = ref<T>();
 
     const fetch = async (fn: (...args: any[]) => Promise<any>, ...args: any[]) => {
         try {
-            errorMessage.value = null;
             isLoading.value = true;
+            errorStore.clearError();
 
             const response = await withMinDelay(() => fn(...args), 1000);
             data.value = response.data;
             return response.data;
         } catch (e: any) {
-            errorMessage.value = e.response?.data?.message || 'Ошибка запроса';
+            const message = e.response?.data?.message || 'Ошибка запроса';
+            errorStore.showError(message);
             throw e;
         } finally {
             isLoading.value = false;
         }
     };
 
-    return { isLoading, errorMessage, data, fetch };
+    return { isLoading, data, fetch };
 }
