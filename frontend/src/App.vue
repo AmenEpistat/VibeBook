509fe127f63811162f225f3e91c333fbe4c719f0
luiza <luizaxakieva@gmail.com>
2026-01-19 23:07:02 +0300

    adds the error modal for app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,10 @@
     <Header v-if="auth" />
     <router-view />
     <Footer />
+    <ErrorModal
+        v-model="errorStore.isVisible"
+        :error="errorStore.message"
+    />
 </template>
 
 <script setup lang="ts">
@@ -9,7 +13,10 @@ import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
 import { useAuthStore } from '@/stores/authStore.ts';
 import { computed, provide } from 'vue';
+import { useErrorStore } from '@/stores/errorStore';
+import ErrorModal from '@/components/ErrorModal.vue';
 
+const errorStore = useErrorStore();
 const authStore = useAuthStore();
 
 authStore.checkAuth();
